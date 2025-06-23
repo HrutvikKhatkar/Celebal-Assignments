37004f7031f03f358e3a9f26343918a5d810a21d
HrutvikKhatkar <hrutvikkhatkar25@gmail.com>
2025-06-23 11:34:33 +0530

    Add CRUD application with MongoDB

---
+++ assignments/mongo-crud-app/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/users/', userRoutes);
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+.then(()=>{
+    console.log('Connected to MongoDB');
+
+    app.listen(process.env.port, ()=>{
+        console.log(`Server running on https://localhost:${process.env.port}`)
+    })
+})
+.catch( (err) => {
+    console.log('MongoDB connection error:', err);
+})
